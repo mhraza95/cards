00d5ec838129d15bbc01b267d76bb3496c6f6fbf
Hassan Raza <mhraza95@gmail.com>
2024-10-26 18:31:23 +0530

    Cards Microservice

---
+++ src/main/java/org/coderzpy/cards/audit/AuditAwareImpl.java
@@ -0,0 +1,12 @@
+package org.coderzpy.cards.audit;
+
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+
+public class AuditAwareImpl implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("CARDS_MS");
+    }
+}
---
+++ src/main/java/org/coderzpy/cards/exception/CardAlreadyExistsException.java
@@ -0,0 +1,12 @@
+package org.coderzpy.cards.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class CardAlreadyExistsException extends RuntimeException{
+    public CardAlreadyExistsException(String message){
+        super(message);
+    }
+}
