57ff2357857936673cb21a2f40c01f8571b580d2
Hassan Raza <mhraza95@gmail.com>
2024-11-07 13:45:33 +0530

    MySQL DB config

--- src/main/java/org/coderzpy/cards/CardsApplication.java
+++ src/main/java/org/coderzpy/cards/CardsApplication.java
@@ -4,8 +4,10 @@ import org.coderzpy.cards.dto.CardsContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @EnableConfigurationProperties(value = {CardsContactInfoDto.class})
 public class CardsApplication {
 
--- src/main/java/org/coderzpy/cards/entity/BaseEntity.java
+++ src/main/java/org/coderzpy/cards/entity/BaseEntity.java
@@ -1,8 +1,8 @@
 package org.coderzpy.cards.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -10,10 +10,12 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @ToString
